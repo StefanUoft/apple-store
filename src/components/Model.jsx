f16c0b8b333dfdcd5e1c3cc77db0bc8ea63e8036
Stefan <stefan.chen@mail.utoronto.ca>
2025-01-18 17:26:53 -0500

    finish model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,12 +1,14 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import React, { useRef, useState } from "react";
 import ModelView from "./ModelView";
+import { useEffect, useRef, useState } from "react";
 import { yellowImg } from "../utils";
+
 import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
-import { Canvas } from "@react-three/fiber";
+import { animateWithGsapTimeline } from "../utils/animations";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -19,18 +21,35 @@ const Model = () => {
   // camera control for the model view
   const cameraControlSmall = useRef();
   const cameraControlLarge = useRef();
+
   // model
   const small = useRef(new THREE.Group());
   const large = useRef(new THREE.Group());
+
   // rotation
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
 
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if (size === "large") {
+      animateWithGsapTimeline(tl, small, smallRotation, "#view1", "#view2", {
+        transform: "translateX(-100%)",
+        duration: 2,
+      });
+    }
+
+    if (size === "small") {
+      animateWithGsapTimeline(tl, large, largeRotation, "#view2", "#view1", {
+        transform: "translateX(0)",
+        duration: 2,
+      });
+    }
+  }, [size]);
+
   useGSAP(() => {
-    gsap.to("#heading", {
-      y: 0,
-      opacity: 1,
-    });
+    gsap.to("#heading", { y: 0, opacity: 1 });
   }, []);
   return (
     <section className="common-padding">
@@ -38,6 +57,7 @@ const Model = () => {
         <h1 id="heading" className="section-heading">
           Take a closer look.
         </h1>
+
         <div className="flex flex-col items-center mt-5">
           <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
             <ModelView
@@ -49,6 +69,7 @@ const Model = () => {
               item={model}
               size={size}
             />
+
             <ModelView
               index={2}
               groupRef={large}
@@ -58,14 +79,15 @@ const Model = () => {
               item={model}
               size={size}
             />
+
             <Canvas
               className="w-full h-full"
               style={{
                 position: "fixed",
-                top: "0",
-                bottom: "0",
-                left: "0",
-                right: "0",
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
                 overflow: "hidden",
               }}
               eventSource={document.getElementById("root")}
@@ -81,18 +103,22 @@ const Model = () => {
                   <li
                     key={i}
                     className="w-6 h-6 rounded-full mx-2 cursor-pointer"
-                    style={{
-                      backgroundColor: item.color[0],
-                    }}
+                    style={{ backgroundColor: item.color[0] }}
                     onClick={() => setModel(item)}
                   />
                 ))}
               </ul>
               <button className="size-btn-container">
                 {sizes.map(({ label, value }) => (
-                  <span key={label} className="size-btn" 
-                  style={{ backgroundColor : size === value ? 'white' : 'transparent',
-                    color: size === value ? 'black' : 'white'}} onClick={() => setSize(value)}>
+                  <span
+                    key={label}
+                    className="size-btn"
+                    style={{
+                      backgroundColor: size === value ? "white" : "transparent",
+                      color: size === value ? "black" : "white",
+                    }}
+                    onClick={() => setSize(value)}
+                  >
                     {label}
                   </span>
                 ))}
